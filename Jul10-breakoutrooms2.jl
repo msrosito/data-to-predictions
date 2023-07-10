6f197a7dc665aa801ae6bbb307c1007647cdbb3d
M. S. Rosito <msrosito@gmail.com>
2023-07-09 23:36:26 -0400

    breakout rooms activity

---
+++ Jul10-breakoutrooms2.jl
@@ -0,0 +1,25 @@
+### A Pluto.jl notebook ###
+# v0.19.26
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 2ad07798-1ed2-11ee-399a-aba41c293077
+md"""
+Compute the following using doble for loop
+
+$\sum_{i=1}^{N} \sum_{j=1}^{M} (i+j)^2$ where N = 5, and M = 3
+
+"""
+
+# ╔═╡ 52dbbaf2-6919-4773-b8db-453fbd6026df
+begin
+	## define M and N
+	## define acc_2
+	## compute the final result
+	acc_2 
+end
+
+# ╔═╡ Cell order:
+# ╟─2ad07798-1ed2-11ee-399a-aba41c293077
+# ╠═52dbbaf2-6919-4773-b8db-453fbd6026df
