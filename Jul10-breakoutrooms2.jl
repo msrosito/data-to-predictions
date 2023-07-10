a54b55ca35fd0c8f04914b93553dd2a86b70b628
M. S. Rosito <msrosito@gmail.com>
2023-07-10 12:50:03 -0400

    change name

--- Jul10-breakoutrooms2.jl
+++ Jul10-breakoutrooms2.jl
@@ -15,9 +15,9 @@ $\sum_{i=1}^{N} \sum_{j=1}^{M} (i+j)^2$ where N = 5, and M = 3
 # ╔═╡ 52dbbaf2-6919-4773-b8db-453fbd6026df
 begin
 	## define M and N
-	## define acc_2
+	## define acc
 	## compute the final result
-	acc_2 
+	acc 
 end
 
 # ╔═╡ Cell order:
